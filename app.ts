7212a79f7040aabdfa73e392bb25305f511ba7bb
Ben Halverson <benhalverson33@gmail.com>
2015-08-20 20:47:10 -0700

    hello world\!

---
+++ app.ts
@@ -0,0 +1,20 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import{
+  Component, View, bootstrap
+} from "angular2/angular2";
+
+@Component({
+  selector: 'hello-world'
+  })
+@View({
+  template: '<article>Hello {{ name }} </article>'
+  })
+class HelloWorld{
+    name: string;
+    constructor() {
+        this.name = 'Ben';
+      }
+  }
+
+  bootstrap(HelloWorld);
